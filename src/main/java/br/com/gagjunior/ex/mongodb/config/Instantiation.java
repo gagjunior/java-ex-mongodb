56ad64804169f1d8f760f26e72a43d91174f8e74
Gilberto Junior <gagjunior@hotmail.com>
2022-11-16 15:45:24 -0300

    Class: Instantiation

---
+++ src/main/java/br/com/gagjunior/ex/mongodb/config/Instantiation.java
@@ -0,0 +1,32 @@
+package br.com.gagjunior.ex.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.gagjunior.ex.mongodb.entities.User;
+import br.com.gagjunior.ex.mongodb.repositories.UserRepository;
+
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    }
+
+}
