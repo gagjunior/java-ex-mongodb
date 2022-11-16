5369bce00e7172b8817574538dbd2a7d7fea868e
Gilberto Junior <gagjunior@hotmail.com>
2022-11-15 23:48:01 -0300

    Create project

---
+++ src/test/java/br/com/gagjunior/ex/mongodb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gagjunior.ex.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
