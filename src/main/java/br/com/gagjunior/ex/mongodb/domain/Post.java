a8d83949bbf6963722b7db4c4d2f29471f6b7c0f
Gilberto Junior <gagjunior@hotmail.com>
2022-11-17 23:32:43 -0300

    Referenciando os posts do usuario na classe 'User'

--- src/main/java/br/com/gagjunior/ex/mongodb/domain/Post.java
+++ src/main/java/br/com/gagjunior/ex/mongodb/domain/Post.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-@Document
+@Document(collection = "post")
 public class Post implements Serializable {
 
     @Id
